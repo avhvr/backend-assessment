79a442672571272c4131d7e6b9d261e8036e3fd6
Venkata Harshavardhan Reddy Allu <venkataharshavardhan_ven@srmuniv.edu.in>
2020-02-06 18:47:18 +0530

    services: add user login and register services

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,9 +1,18 @@
+import logging
+from models import User
+from flask import jsonify
+from tasks import user_task
+
+logger = logging.getLogger("default")
+
+
 class UserService(object):
     """
     service function for user related business logic
     """
 
-    def login_user(self):
+    @staticmethod
+    def login_user(data):
         """
         TASKS: write the logic here for user login
                authenticate user credentials as per your
@@ -11,5 +20,55 @@ class UserService(object):
 
                raise appropriate errors wherever necessary
         """
+        if data:
+            username = data.get('username')
+            password = data.get('password')
+            if username and password:
+                user = User.objects(username=username).first()
+                if user is not None and user.verify_password(password):
+                    logger.info(f"'{user.id}' is authenticated")
+                    user_task.lastlogin.delay(user.id)
+                    return jsonify({
+                        "message": f"'{user.id}' is authenticated",
+                    }), 200
+                return jsonify({
+                    "messaget": "Invalid username or password!"
+                }), 401
+            return jsonify({
+                "messaget": "Empty username or password!"
+            }), 400
+        return jsonify({
+            "message": "No JSON data found in the request!"
+        }), 400
 
-        pass
+
+
+    @staticmethod
+    def register_user(data):
+        """
+        TASKS: write the logic here for registering new user.
+
+               raise appropriate errors wherever necessary
+        """
+        if data:
+            username = data.get('username')
+            password = data.get('password')
+            if username and password:
+                user = User.objects(username=username).first()
+                if user is None:
+                    user = User(username)
+                    user.password = password
+                    user.save()
+                    logger.info(f"Registering new user '{user.id}'")
+                    return jsonify({
+                        "message": f"'{user.id}' is registered",
+                    }), 201
+                return jsonify({
+                    "message": f"'{username}' already exists!"
+                }), 400
+            return jsonify({
+                "message": f"Empty username or password!"
+            }), 400
+        return jsonify({
+            "message": "No JSON data found in the request!"
+        }), 400
